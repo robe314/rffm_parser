cae49608184353795a26a724de1a586e2dac588d
robe314 <robe.garcia@gmail.com>
2024-02-21 19:06:52 +0100

    test bot

--- text_horario.py
+++ text_horario.py
@@ -1,5 +1,5 @@
 from text_dict import text
-from horario import get_horario
+from horarios import get_horarios
 
 myurl = "https://www.rffm.es/competicion/calendario?temporada=19&tipojuego=1&competicion=17145553&grupo=17145567"
 
