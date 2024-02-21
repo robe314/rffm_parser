66fdd586a960d4a2ee6a4c0406c7fd82487056c8
robe314 <robe.garcia@gmail.com>
2024-02-21 18:08:54 +0100

    Modified html parser

--- web_parser.py
+++ web_parser.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 def get_dict(url):
     temp=urllib.request.urlopen(url)
     html=temp.read()
-    soup = BeautifulSoup(html,features="lxml")
+    soup = BeautifulSoup(html,features="html.parser")
     datos=soup.body.script.contents[0]
     mydict = json.loads(datos)
     return mydict
