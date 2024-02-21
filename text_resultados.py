78902e74dfd7ebf368784f644c0ad2f2561f3b8f
robe314 <robe.garcia@gmail.com>
2024-02-21 19:16:10 +0100

    test bot

--- text_resultados.py
+++ text_resultados.py
@@ -3,7 +3,7 @@ from resultados import get_resultados
 
 myurl = "https://www.rffm.es/competicion/calendario?temporada=19&tipojuego=1&competicion=17145553&grupo=17145567"
 
-def text_clasificacion():
+def text_resultados():
     myresultados=get_resultados(myurl)
     texto=text(myresultados)
     return texto
