c93afc6c49bb8ddad1eef6476da5619cbccb3641
robe314 <robe.garcia@gmail.com>
2024-02-21 18:57:08 +0100

    test bot

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,6 +1,9 @@
 import telebot
 from text_dict import text
 from horarios import get_horarios
+from text_clasificacion import text_clasificacion
+
+text_clasificacion()
 
 myurl = "https://www.rffm.es/competicion/calendario?temporada=19&tipojuego=1&competicion=17145553&grupo=17145567"
 horario=text(get_horarios(myurl))
@@ -9,9 +12,13 @@ bot = telebot.TeleBot("6822504022:AAH2z4sNp4GJBRy8YHXmRCBvGnrQfbOlecs")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-  bot.reply_to(message, "Escribe /horario o /cuartos")
+  bot.reply_to(message, "Escribe /horario, /cuartos, /clasificacion")
 
 @bot.message_handler(commands=['horario','Horario'])
+def echo_all(message):
+  bot.reply_to(message, horario)
+
+@bot.message_handler(commands=['clasificacion','Clasificacion'])
 def echo_all(message):
   bot.reply_to(message, horario)
 
---
+++ text_clasificacion.py
@@ -0,0 +1,8 @@
+from text_dict import text
+from clasificacion import get_clasificacion
+
+myurl = "https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145567"
+
+def text_clasificacion():
+    myclasif=text(myurl)
+    return myclasif
