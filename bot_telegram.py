aa4641309eac376a30f09d8bf888f461cfec3a53
robe314 <robe.garcia@gmail.com>
2024-02-21 19:14:48 +0100

    test bot

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,15 +1,17 @@
 import telebot
 from text_clasificacion import text_clasificacion
 from text_horario import text_horario
+from text_resultados import text_resultados
 
 clasificacion=text_clasificacion()
 horario=text_horario()
+resultados=text_resultados()
 
 bot = telebot.TeleBot("6822504022:AAH2z4sNp4GJBRy8YHXmRCBvGnrQfbOlecs")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-  bot.reply_to(message, "Escribe /horario, /cuartos, /clasificacion")
+  bot.reply_to(message, "Escribe /horario, /resultados, /clasificacion")
 
 @bot.message_handler(commands=['horario','Horario'])
 def echo_all(message):
@@ -19,8 +21,8 @@ def echo_all(message):
 def echo_all(message):
   bot.reply_to(message, clasificacion)
 
-@bot.message_handler(commands=['cuartos','Cuartos'])
+@bot.message_handler(commands=['resultados','Resultados'])
 def echo_all(message):
-  bot.reply_to(message, get_table())
+  bot.reply_to(message, resultados)
 
 bot.infinity_polling()
---
+++ text_resultados.py
@@ -0,0 +1,9 @@
+from text_dict import text
+from resultados import get_resultados
+
+myurl = "https://www.rffm.es/competicion/calendario?temporada=19&tipojuego=1&competicion=17145553&grupo=17145567"
+
+def text_clasificacion():
+    myresultados=get_resultados(myurl)
+    texto=text(myresultados)
+    return texto
