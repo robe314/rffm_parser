29cc6b33ba7ce24a2ca971ffcc8f35cd73180d28
robe314 <robe.garcia@gmail.com>
2024-02-23 16:33:47 +0100

    Add variables file

---
+++ variables.py
@@ -0,0 +1,30 @@
+# Fichero para establecer variables
+
+# URL de la clasificacion de la competicion y grupo a seguir
+url_clasificacion = "https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145567"
+
+# URL del calendario de la competicion y grupo a seguir
+url_calendario = "https://www.rffm.es/competicion/calendario?temporada=19&tipojuego=1&competicion=17145553&grupo=17145567"
+
+# URLs del calendario de los equipos a seguir
+url_seguidos = ["https://www.rffm.es/competicion/calendario?temporada=19&tipojuego=1&competicion=17145553&grupo=17145567",
+        "https://www.rffm.es/competicion/calendario?temporada=19&tipojuego=1&competicion=17145553&grupo=17145568",
+        "https://www.rffm.es/competicion/calendario?temporada=19&tipojuego=1&competicion=17145570&grupo=17145602"]
+
+# URLs de las clasificaciones de la competicion a seguir para calcular el coeficiente de los equipos que ocupan el cuarto puesto
+url_cuartos=["https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145554",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145555",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145556",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145557",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145558",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145559",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145560",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145561",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145562",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145563",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145564",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145565",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145566",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145567",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145568",
+"https://www.rffm.es/competicion/clasificaciones?temporada=19&competicion=17145553&grupo=17145569"]
